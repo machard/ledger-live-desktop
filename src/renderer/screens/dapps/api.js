1f95702c8f22e1ce526dae67addd5ca2d0783f61
machard <matthieu__@hotmail.fr>
2021-03-17 22:36:02 +0100

    remove useless

--- src/renderer/screens/dapps/api.js
+++ src/renderer/screens/dapps/api.js
@@ -6,12 +6,10 @@ import { shallowAccountsSelector } from "~/renderer/reducers/accounts";
 const app = express();
 
 app.get("/spec.json", (req, res) => {
-  console.log("coucou", res);
   res.json({ status: 200, body: spec });
 });
 
 app.get("/v0.0.1/accounts", (req, res) => {
-  console.log("COUCOU");
   res.json({ status: 200, body: shallowAccountsSelector(store.getState()) });
 });
 
